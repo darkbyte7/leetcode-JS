c3bd879fd8644753e36fff90882c92f0b21ef770
Mihir Mahakalkar <darkbyte7@macbook-pro.local>
2022-07-17 21:18:34 +0530

    LC242 solution

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -1,3 +1,22 @@
-function isAnagram(s, t) {}
+function isAnagram(s, t) {
+    // if lengths of s and t are unequal, then not anagram.
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const sCharsMap = {}; // { H:1, E:1, L:2, O:1}
+
+    // populate character map of s - sCharsMap
+    for (let c of s) {
+        sCharsMap[c] = (sCharsMap[c] || 0) + 1;
+    }
+
+    for (let c of t) {
+        if (!sCharsMap[c]) return false;
+        sCharsMap[c]--;
+    }
+
+    return true;
+}
 
 module.exports = isAnagram;
